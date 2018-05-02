1900434e0d7bb29fcdaf182f50722fba8fd8ed25
tnahcrem <hzm1709@gmail.com>
2018-05-02 11:36:57 -0700

    Add files via upload

---
+++ config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// DEFINES
+#define NUM_VOLUNTEERS 14
+#define NUM_MIQAATS	32
+
+/* optional indices to skip particular JV
+the indices start from 0
+if you dont wast to skip assign it 9999 so it is invalid
+*/
+#define SKIP_JV1_A 22
+#define SKIP_JV1_B 9999 // invalid
+#define SKIP_JV2_A 21
+#define SKIP_JV2_B 22
+#define SKIP_JV3_A 21
+#define SKIP_JV3_B 22
