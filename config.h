b1216846053cc21f6a6673430901ad21d6405042
tnahcrem <hzm1709@gmail.com>
2018-07-06 12:28:53 -0700

    support for sabeel

--- config.h
+++ config.h
@@ -2,17 +2,17 @@
 
 // DEFINES
 #define NUM_VOLUNTEERS 14
-#define NUM_MIQAATS	32
+#define NUM_MIQAATS	9
 
 /* optional indices to skip particular JV
 the indices start from 0
 if you dont wast to skip assign it 9999 so it is invalid
-for e.g. in SHerullah - LQ night we want to skip JV2 and JV3
-on LQ sehori we want to skip all JVs
 */
-#define SKIP_JV1_A 22
+#define SKIP_JV1_A 9999
 #define SKIP_JV1_B 9999 // invalid
-#define SKIP_JV2_A 21
-#define SKIP_JV2_B 22
-#define SKIP_JV3_A 21
-#define SKIP_JV3_B 22
+#define SKIP_JV2_A 9999
+#define SKIP_JV2_B 9999
+#define SKIP_JV3_A 9999
+#define SKIP_JV3_B 9999
+
+#define DO_SABEEL 1
